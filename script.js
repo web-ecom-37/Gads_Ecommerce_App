3382542dfb4392fb29787066b4f46c897a02dc50
Prince Darko <padarko99@gamil.com>
2020-10-22 17:55:40 -0700

    updated script2.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-console.log('prince');
-
 let butt = document.querySelectorAll('.content button');
-
-// let table = document.querySelector('#tables')
-// console.log(table.childNode[1])
 
 butt.forEach(myfunction)
 
@@ -11,19 +6,16 @@ function myfunction(button){
     button.addEventListener('click', () => {
         let content = {}
         let name = button.previousSibling.previousSibling.previousSibling.previousSibling.innerHTML;
-        console.log(name)
-        // content.push(name)
+       
         content.name = name;
         let price = button.previousSibling.previousSibling.innerHTML
-        // console.log(name)
-        // content.push(price)
-        // console.log(content)
-        content.price = price;
         
-        console.log(content)
-        // console.log(content.length)
+        content.price = price;
+        let img = button.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.previousSibling.src
+        content.img = img
         addToLocaleStorage(content)
-       
+
+
         
     })
 }
--- script2.js
+++ script2.js
@@ -19,7 +19,7 @@ todos.forEach(item => {
     for(let i = 0; i < 4; i++){
         td[i]  = document.createElement('td');
     };
-    
+    td[0].innerHTML = `<img src="${item.img}" alt="model">`
     td[1].innerHTML = `${item.name}`
     
     td[2].innerHTML = `${item.price}`
@@ -31,13 +31,33 @@ todos.forEach(item => {
     })
 
     
+    
     table.appendChild(row)
     
     
     
+    
 });
+    //calculate total amount
+    let amount = []
+    todos.forEach(todo =>{
+        amount.push(todo.price)
+    })
+    
+   
+
+    total(todos, amount, getSum, table)
+
+    
+
 
 })();
+
+// calculating total amount
+function getSum(total, num){
+    return total + Math.round(num)
+}
+
 
 
 // delete item from local storage
@@ -54,15 +74,16 @@ clear.forEach(item => {
         del_item.price = price
         let removed = item.parentElement.parentElement
         removed.classList.add('hide');
-        
 
-
-        console.log(typeof(del_item))
         removeFromStoreage(del_item)
 
         
     } )
 })
+
+
+let total_items = document.querySelector('.total_items')
+let total_price = document.querySelector('.total_price')
 
 
 function removeFromStoreage(todo){
@@ -76,11 +97,46 @@ function removeFromStoreage(todo){
     
  
     let undeleted_items = todos.filter(x => x.name !== todo.name)
-  
-    
     localStorage.setItem('todos', JSON.stringify(undeleted_items))
 
+
+
+// setting current items and price
+    let remaining_amount = []
+    undeleted_items.forEach(amount =>{
+        remaining_amount.push(amount.price)
+    })
+  
+    
+    total_items.innerHTML = undeleted_items.length
+    total_price.innerHTML = remaining_amount.reduce(getSum, 0)
+
  
+
+}
+
+
+function total(todos,amount, getSum, table){
+
+    let total_row = document.createElement('tr');
+    let total_items  = document.createElement('td');
+    let total_items_num  = document.createElement('td');
+    let total_price  = document.createElement('td');
+    let total_price_num  = document.createElement('td');
+    total_items.innerHTML = `Total Items`
+    total_items_num.innerHTML = todos.length
+    total_price.innerHTML = `Total Price`
+    total_price_num.innerHTML = amount.reduce(getSum, 0)
+    total_row.appendChild(total_items)
+    total_row.appendChild(total_items_num)
+    total_row.appendChild(total_price)
+    total_row.appendChild(total_price_num)
+    
+    table.appendChild(total_row)
+
+    total_items_num.classList.add('total_items')
+    total_price_num.classList.add('total_price')
+
 
 }
 
